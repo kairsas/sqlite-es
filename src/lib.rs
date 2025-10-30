59465b9240abb422e9b86a21a9f2fa19889ae20b
kairsas <kairius@gmail.com>
2025-10-30 15:35:17 +0200

    chore: add examples on README

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 #![deny(missing_docs)]
 #![deny(clippy::all)]
 #![warn(rust_2018_idioms)]
+#![doc = include_str!("../README.md")]
 //! # sqlite-es
 //!
 //! > A Sqlite implementation of the `EventStore` trait in [cqrs-es](https://crates.io/crates/cqrs-es).
@@ -11,12 +12,13 @@ pub use crate::event_repository::*;
 pub use crate::init::*;
 pub use crate::types::*;
 pub use crate::view_repository::*;
+pub use crate::testing::*;
 
 mod cqrs;
 mod error;
 mod event_repository;
 mod init;
 pub(crate) mod sql_query;
-mod testing;
+pub(crate) mod testing;
 mod types;
 mod view_repository;
